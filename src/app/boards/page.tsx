24c45464dad3140cf738f61ac7c6b8cb73c520b3
Sam Hill <soshjam@soshjam.com>
2024-03-05 02:08:27 -0700

    Move board fetching into client-side component

--- src/app/boards/page.tsx
+++ src/app/boards/page.tsx
@@ -1,10 +1,18 @@
 import styles from "./page.module.scss";
+import { Metadata } from "next";
+import { BoardList } from "@/components/boardlist";
 
-export default async function BoardPage({ params }: Readonly<{ params: { id: string } }>) {
+export const metadata: Metadata = {
+    title: "Boards | Bulletinator",
+    description: "Bulletinator is a bulletin board app for planning your next project.",
+};
+
+export default function BoardListPage({ params }: Readonly<{ params: { id: string } }>) {
 
     return (
         <main>
             <h1>Boards</h1>
+            <BoardList params={params}/>
         </main>
     );
 }
---
+++ src/components/boardlist.tsx
@@ -0,0 +1,33 @@
+'use client'
+
+import styles from "./boardlist.module.scss";
+import { useState, useEffect } from "react";
+import { Board } from '@/lib/types';
+
+export function BoardList({ params }: Readonly<{ params: { id: string } }>) {
+
+    const [boards, setBoards] = useState<Board[]>([]);
+
+    const getBoardList = async (email: string) => fetch(`/api/get-boards/user/${email}`) // Get the list of board IDs
+        .then(response => response.json()) // Parse to a JSON list of board IDs
+        .then((boardIds: string[]) => Promise.all( // Get the info of each board
+            boardIds.map(id => fetch(`/api/get-boards/${id}`) // Get the board info
+                .then(response => response.json()) // Parse to a JSON board
+            )
+        ))
+        .then((boards: Board[]) => setBoards(boards)) // Set the state
+
+    useEffect(() => {
+        getBoardList("john@example.com"); // TODO: Replace with user email after authentication is implemented
+    });
+
+    return (
+        <ul>
+            {boards.map((board: Board) => (
+                <li key={board._id}>
+                    {board.title}
+                </li>
+            ))}
+        </ul>
+    );
+}
